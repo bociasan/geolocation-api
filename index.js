96df5504992e4202f795b864523794c0904a3c91
bociasan <bociasan@gmail.com>
2023-05-20 02:14:43 +0300

    Added map.

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-const express = require('express');
+import {WebSocketServer} from 'ws';
+import express from 'express';
+
 const app = express();
 const port = 3000
+const wss = new WebSocketServer({port: 80});
+let state = {millis: 0}
+let millisFlag = true
+const millisFunc = () => {
+    if (millisFlag){
+        setTimeout(() => {
+            state.millis++
+            wss.clients.forEach(function each(ws) {
+                if (ws.isAlive === false) return ws.terminate();
+                ws.send(JSON.stringify(state));
+            });
+            // console.log(millis)
+            millisFunc()
+        }, 1000)
+    }
+}
+millisFunc()
+const startMillis = () => {
+    millisFlag = true
+    millisFunc()
+}
+const stopMillis = () => {
+    millisFlag = false
+}
 
-app.get('/', function(request, response){
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+});
+app.get('/', function (request, response) {
     response.sendFile("D:\\Projects\\license_thesis\\geolocation-api\\index.html");
 });
 app.listen(port, () => {
