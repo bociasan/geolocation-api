f3f47008a2d9fa7a3f0070d55f38ec4a02d4c792
bociasan <bociasan@gmail.com>
2023-05-20 03:12:52 +0300

    Position of phone is updating on map. Points are generating on phone position.

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ const stopMillis = () => {
 
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
-
     ws.on('message', function message(data) {
         console.log('received: %s', data);
     });
